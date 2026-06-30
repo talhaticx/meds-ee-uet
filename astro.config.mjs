6e5b542b020dbb4a93919f9890ac58341c3c0988
talhaticx <mtalhaayaz2005@gmail.com>
2026-06-30 14:48:10 +0500

    github page feat added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import netlify from '@astrojs/netlify';
 import keystatic from '@keystatic/astro';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  // 1. Set output to static (required for GitHub Pages)
+  output: 'static',
+  
+  // 2. Define your GitHub Pages URL and repo name
+  site: 'https://talhaticx.github.io',
+  base: 'meds-ee-uet',
+
   integrations: [react(), markdoc(), keystatic()],
 
   vite: {
